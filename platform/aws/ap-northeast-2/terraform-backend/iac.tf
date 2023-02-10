190efce8923dc3095e4b9847ea9e97c749483b79
Duckie1 <mdh8023@gmail.com>
2023-02-10 18:46:28 +0900

    upload1846

--- platform/aws/ap-northeast-2/terraform-backend/iac.tf
+++ platform/aws/ap-northeast-2/terraform-backend/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-backend" {
-  source = "git@github.com:<Github Profile명>/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
+  source = "git@github.com:Duckie1/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
 }
