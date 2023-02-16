1b1d94b9248b03028a9d4a8422d8382c015aa072
Duckie1 <mdh8023@gmail.com>
2023-02-16 17:46:10 +0900

    edit1746

--- platform/aws/ap-northeast-2/terraform-codes/iac.tf
+++ platform/aws/ap-northeast-2/terraform-codes/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-codes" {
-  source = "git@github.com:<Github Profile명>/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
+  source = "git@github.com:Duckie1/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
 }
